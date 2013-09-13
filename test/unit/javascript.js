56afeea37e94c288090559f2bc8b66c85e74946e
unnoon <r.geertzema@gmail.com>
2013-09-13 16:08:06 +0200

    first commit

---
+++ test/unit/javascript.js
@@ -0,0 +1,128 @@
+/*
+ * Just a reminder of how Javascript handles certain things
+ */
+
+
+describe("Extensions", function() {
+
+	describe("Javascript", function() {
+
+		describe("logical evaluations", function() {
+
+			it("strings", function() {
+				expect('0').to.be.ok;
+				expect(-6).to.be.ok;
+				expect(6).to.be.ok;
+				expect(Infinity).to.be.ok;
+				expect([]).to.be.ok;
+				expect({}).to.be.ok;
+				expect(function(){}).to.be.ok;
+
+				expect(0).to.not.be.ok;
+				expect("").to.not.be.ok;
+				expect(NaN).to.not.be.ok;
+				expect(null).to.not.be.ok;
+				expect(undefined).to.not.be.ok;
+			});
+		});
+
+		describe("enumerations", function() {
+			var superProto = Object.create(Object.prototype);
+			var proto      = Object.create(superProto);
+			var obj        = Object.create(proto);
+
+			Object.defineProperty(obj, 'a1', {value: 'a1', enumerable: true});
+			Object.defineProperty(obj, 'b1', {value: 'b1', enumerable: false});
+			Object.defineProperty(proto, 'a2', {value: 'a2', enumerable: true});
+			Object.defineProperty(proto, 'b2', {value: 'b2', enumerable: false});
+			Object.defineProperty(superProto, 'a3', {value: 'a3', enumerable: true});
+			Object.defineProperty(superProto, 'b3', {value: 'b3', enumerable: false});
+
+			it("for in: retrieves all 'enumerable' properties over de prototype chain", function() {
+				var props = [];
+
+				for(var prop in obj)
+				{
+					props.push(prop);
+				}
+
+				expect(props).to.eql(['a1', 'a2', 'a3']);
+			});
+
+			it("for in + hasOwnProperty: retrieves all 'enumerable' properties off the object itself", function() {
+				var props = [];
+
+				for(var prop in obj)
+				{
+					if(obj.hasOwnProperty(prop)) props.push(prop);
+				}
+
+				expect(props).to.eql(['a1']);
+			});
+
+			it("Object.keys: retrieves all 'enumerable' properties off the object itself", function() {
+				expect(Object.keys(obj)).to.eql(['a1']);
+			});
+			// NOTE this is the only way to get non enumarable properties
+			it("Object.get: retrieves all properties of the object itself", function() {
+				expect(Object.getOwnPropertyNames(obj)).to.eql(['a1', 'b1']);
+			});
+		});
+
+		describe("typeof", function() {
+
+			it("javascript types", function() {
+				expect(typeof 6).to.eql('number');
+				expect(typeof NaN).to.eql('number');
+				expect(typeof Infinity).to.eql('number');
+				expect(typeof 's').to.eql('string');
+				expect(typeof []).to.eql('object'); // counter intuitive
+				expect(typeof {}).to.eql('object');
+				expect(typeof function(){}).to.eql('function');
+				expect(typeof null).to.eql('object'); // counter intuitive. Although only objects can have the value null...
+				expect(typeof undefined).to.eql('undefined');
+			});
+
+			it("custom types: named constructor", function() {
+				function Animal() {} // make sure the constructor is a named function
+				var animal = new Animal();
+
+				expect(typeof animal).to.eql('object');
+			});
+
+			it("custom types: UNnamed constructor", function() {
+				var Cat = function () {};  // unnamed constructor
+				var cat = new Cat();
+
+				expect(typeof cat).to.eql('object');
+			});
+		});
+
+		describe("closures", function() {
+
+			it("vars in closures continue to work as normal vars", function() {
+				var setNummie;
+
+				var clo = (function() {
+				    var nummie = 555;
+
+					var fnc = function() {
+						return nummie;
+					};
+
+					setNummie = function(val) {
+						nummie = val;
+					};
+
+					return fnc;
+				})();
+
+				expect(clo()).to.eql(555);
+
+				setNummie(666);
+
+				expect(clo()).to.eql(666);
+			});
+		});
+	});
+});
