79392cbcb738e86492e23cf69edb580846c9a5af
rogier.geertzema <rogier.geertzema@boostermedia.com>
2014-08-25 11:18:16 +0200

    - added distance tests

--- test/unit/misc.js
+++ test/unit/misc.js
@@ -1,13 +1,29 @@
+function asyncFunction(cb) {
+    setTimeout(function() {
+        cb("super strange error message shown");
+    }, 1999)
+}
+
 describe("Misc", function() {
 
-//	describe("async", function() {
+	describe("async", function() {
 
-    xit("simple", function(done) {
-        setTimeout(function() {
-            assert.equal(false, true);
-            done();
-        }, 50)
-    });
+        it("simple", function(done) {
+            setTimeout(function() {
+                assert.equal(false, true);
+                done();
+            }, 50)
+        });
 
-//	});
+        it("async nested", function(done) {
+            asyncFunction(function(err) {
+//                assert.equal(false, true, "false is not true?!?!");
+                assert.isNull(err, err);
+//                expect(true).to.be.false;
+
+                expect(false).to.be.true;
+                done();
+            })
+        });
+	});
 });
