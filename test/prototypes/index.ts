d2fb3c916f0d1aa6a5f4f81de71ebfb2e17863ff
grogger.io <r.geertzema@gmail.com>
2017-09-11 22:39:22 +0200

    lasted test code.

--- test/prototypes/index.ts
+++ test/prototypes/index.ts
@@ -1,14 +1,34 @@
 /**
  * Created by Rogier on 13/04/2017.
  */
+/* tslint:disable: max-classes-per-file no-console */
 
-import { clone } from 'lodash';
-import Value from '../../src/Value';
-import XDArray from '../../src/XDArray';
+import 'reflect-metadata';
+import * as is from '../../src/lang/is';
+// import { decorator }              from '../../src/decorators/decorator';
+import { KeyPropertyDescriptor } from '../../src/classes/KeyPropertyDescriptor';
 
-const fnValue = () => 3;
-const init    = Value.of(fnValue);
-const xdarray = new XDArray([3, 2, 4], init);
+class TestDefineProperty
+{
+    public static id = 11;
 
-const o = clone(fnValue);
-console.log(xdarray[1][0][2](), 666);
+    public static method() { return 'do moar stuff';}
+
+    public id;
+
+    constructor()
+    {
+        this.id = 10;
+    }
+
+    public method() { return 'do stuff';}
+}
+
+const td = new TestDefineProperty();
+
+const dsc1 = Object.getOwnPropertyDescriptor(TestDefineProperty, 'id');
+const dsc2 = Object.getOwnPropertyDescriptor(TestDefineProperty, 'method');
+const dsc3 = Object.getOwnPropertyDescriptor(td, 'id');
+const dsc4 = Object.getOwnPropertyDescriptor(TestDefineProperty.prototype, 'method');
+
+// console.assert(tdp['_id'].hasOwnProperty(), '_id should be defined on instance');
