e07c26f50c53627a772ecdd3ab235d4982133d50
grogger.io <r.geertzema@gmail.com>
2017-09-14 12:50:27 +0200

    adapted to relocation of tmp folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: './test/prototypes/index.ts',
     output: {
-        filename: 'bin/tmp/bundle.js',
+        filename: 'tmp/bundle.js',
         libraryTarget: 'umd'
     },
     resolve: {
