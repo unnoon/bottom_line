6dd917cbb25221496e5f3de0af1daf74e326206b
grogger.io <r.geertzema@gmail.com>
2017-09-18 18:28:06 +0200

    added js to extensions

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
         libraryTarget: 'umd'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.jsx', 'json']
+        extensions: ['.js', '.ts', '.tsx', '.jsx', 'json']
     },
     module: {
         loaders: [
